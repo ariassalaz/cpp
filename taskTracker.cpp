a8611c27e0b3d71f0bbfbe8848409dedac9b0cef
Ricardo Arias <ricardoariassalazarc@outlook.com>
2025-10-21 19:30:58 -0600

    Agrega nuevo programa en C++

---
+++ taskTracker.cpp
@@ -0,0 +1,18 @@
+/*Task tracker is a project used to track and manage your tasks. 
+In this task, you will build a simple command line interface (CLI)
+to track what you need to do, what you have done, and what you are currently working on. 
+This project will help you practice your programming skills, 
+including working with the filesystem, handling user inputs,
+and building a simple CLI application.
+
+Requirements
+The application should run from the command line, 
+accept user actions and inputs as arguments.
+The user should be able to:
+
+Add, Update, and Delete tasks
+Mark a task as in progress or done
+List all tasks
+List all tasks that are done
+List all tasks that are not done
+List all tasks that are in progress*/
